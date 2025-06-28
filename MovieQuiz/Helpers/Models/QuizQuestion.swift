d0cd45698bedc4b3fb0e0f12e6f2b7e68a52a3d7
Trudno <trudno@yandex.ru>
2025-06-28 17:41:10 +0400

    Наладил взаимодействия между контроллером и фабрикой; устраненил ошибки

---
+++ MovieQuiz/Helpers/Models/QuizQuestion.swift
@@ -0,0 +1,15 @@
+//
+//  QuizQuestion.swift
+//  MovieQuiz
+//
+//  Created by Николай Доценко on 28.06.2025.
+//
+
+import Foundation
+import UIKit
+
+struct QuizQuestion {
+    let image: String
+    let text: String
+    let correctAnswer: Bool
+}
---
+++ MovieQuiz/Helpers/Models/QuizResultsViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  QuizResultsViewModel.swift
+//  MovieQuiz
+//
+//  Created by Николай Доценко on 28.06.2025.
+//
+
+import Foundation
+import UIKit
+
+struct QuizResultsViewModel {
+    let title: String
+    let text: String
+    let buttonText: String
+}
